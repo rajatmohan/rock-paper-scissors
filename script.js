ed134a655cf66b84efcb307e9782408f684b3f4f
rajatmohan <rajatm2196@gmail.com>
2022-09-17 00:38:03 +0530

    Add a game function to play multiple rounds of game between player and computer and display final result of the game

--- script.js
+++ script.js
@@ -28,3 +28,50 @@ const playRound = (playerSelection, computerSelection) => {
     }
     return -1;
 }
+
+// displayRoundResult function display the result of one round
+const displayRoundResult = (playerSelection, computerSelection, result) => {
+    if(result === 0) {
+        console.log("Tie")
+    }
+    else if(result > 0) {
+        console.log(`You Won! ${playerSelection} beats ${computerSelection}`);
+    }
+    else {
+        console.log(`You Loose! ${computerSelection} beats ${playerSelection}`);
+    }
+}
+
+
+// displayGameResult function display the result of game which consists of many rounds
+const displayGameResult = (playerScore, computerScore, totalRounds) => {
+    if(playerScore == computerScore) {
+        console.log('Game Tie')
+    }
+    if(playerScore > computerScore) {
+        console.log(`You won the game, your won ${playerScore} rounds computer won ${computerScore} out of total ${totalRounds} rounds.`);
+    }
+    else {
+        console.log(`You loose the game, your won ${playerScore} rounds computer won ${computerScore} out of total ${totalRounds} rounds.`);
+    }
+}
+
+const game = ()=> {
+    // playerScore stores number of rounds player won.
+    let playerScore = 0;
+    // computerScore store number of rounds computer won.
+    let computerScore = 0;
+    let totalRounds = 5;
+    for(let round = 1; round <= totalRounds; round++) {
+        let playerSelection = prompt("Enter your choice");
+        let computerSelection = getComputerChoice();
+
+        let result = playRound(playerSelection, computerSelection);
+        displayRoundResult(playerSelection, computerSelection, result);
+        playerScore += ((result > 0)? 1: 0);
+        computerScore += ((result < 0)? 1: 0);
+    }
+    displayGameResult(playerScore, computerScore, totalRounds)
+}
+
+game()
