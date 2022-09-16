07b1e98a6b4bf6cd1baa4f30a62fd650549afa0a
rajatmohan <rajatm2196@gmail.com>
2022-09-17 00:07:04 +0530

    Add a function to play a single round of Rock Paper Scissors

--- script.js
+++ script.js
@@ -3,8 +3,28 @@ const getRandomNumber = (n) => {
     return Math.floor(Math.random()*n);
 }
 
-// getComputerChoice function return computer choices randomly
+// getComputerChoice function return computer choice randomly
 const getComputerChoice = ()=> {
     const choices = ["Rock", "Paper", "Scissors"];
     return choices[getRandomNumber(choices.length)];
+}
+
+/* playRound function return following value
+    -1 -> for computer win
+    0  -> for tie
+    1  -> for player win */
+const playRound = (playerSelection, computerSelection) => {
+    // converting each parameters to lowercase
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
+    if(playerSelection === computerSelection) {
+        return 0;
+    }
+    if((playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper") ) {
+        return 1;
+    }
+    return -1;
 }
